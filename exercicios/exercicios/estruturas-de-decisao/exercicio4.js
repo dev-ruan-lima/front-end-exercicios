7bf62b713f42751ec2660f84141e6d9bfa9eda0a
RuanRochaLima <ru.lima.rocha@gmail.com>
2025-09-15 21:45:23 -0300

    resolvendo os exercicios de js

---
+++ exercicios/exercicios/estruturas-de-decisao/exercicio4.js
@@ -0,0 +1,9 @@
+
+let idade = prompt("Digite sua idade:");
+let renda = prompt("Digite sua renda mensal:");
+
+if (idade >= 18 && renda >= 3000) {
+    alert("Empréstimo aprovado");
+} else {
+    alert("Empréstimo negado");
+}
