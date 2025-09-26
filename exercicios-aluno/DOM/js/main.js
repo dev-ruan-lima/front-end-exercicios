8a1832ef5e49242b22722a6db45b4d2958ade5fe
RuanRochaLima <ru.lima.rocha@gmail.com>
2025-09-25 21:47:15 -0300

    exercicios de js alterando o html e css

---
+++ exercicios-aluno/DOM/js/main.js
@@ -0,0 +1,20 @@
+let cortico = document.getElementById('o-cortico');
+    console.log(cortico);
+
+let objetosA = document.getElementsByTagName('a');
+    console.log(objetosA);
+
+let vidaSeca = document.querySelector('#vidas-secas');
+    console.log(vidaSeca);
+
+let navList = document.querySelector('.footer-links .nav-list');
+    console.log(navList);
+    
+let Navlist = document.querySelector('.header .nav-list');
+    console.log(Navlist);
+
+cortico.style.color = '#993300'
+
+vidaSeca.className = 'card-livro-fora-de-estoque'
+
+navList.style.background = '#fffff9'
